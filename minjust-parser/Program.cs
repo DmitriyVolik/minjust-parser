45757bd11abf608e8c6e0936dea955f162c6903d
DmitriyVolik <60630678+DmitriyVolik@users.noreply.github.com>
2021-12-13 19:58:45 +0200

    init

---
+++ minjust-parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace minjust_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
